5e41e48221899895b9b4a781ef0feaa90ffcd9f7
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-07-04 18:50:07 +0800

    Initial commit

---
+++ thp_mochaccino/example/thp_mochaccino_example.dart
@@ -0,0 +1,6 @@
+import 'package:thp_mochaccino/thp_mochaccino.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ thp_mochaccino/test/thp_mochaccino_test.dart
@@ -0,0 +1,16 @@
+import 'package:thp_mochaccino/thp_mochaccino.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
